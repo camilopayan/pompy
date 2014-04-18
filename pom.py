f39a55b7e41be92a5350fc268db4d09ad7f09117
Camilo Payan <cpayan@gmail.com>
2014-04-17 20:24:39 -0400

    Simplifying as I learn more about rumps. Realizing I did *not* know how anything worked

--- pom.py
+++ pom.py
@@ -1,35 +1,27 @@
 import rumps
 
 
-@rumps.timer(60)
-def pomodoro_update(sender):
-    sender.update_pomodoro()
+class Interval(rumps.Timer):
+
+    interval_length = 1
+
+    def __init__(self, length, callback):
+        super(self, callback, 5)
+        self.interval_length = length
 
 
-@rumps.timer(1)
-def tester(sender):
-    print sender.time_spent
+class Pomodoro(rumps.App):
+    worklength = 10
+    restlength = 5
 
+    def __init__(self):
+        super(Pomodoro, self).__init__('P', menu=['Start Work Cycle'])
+        rumps.debug_mode(True)
 
-pom_interval = 20  # Work time in minutes
-break_interval = 5  # Break time in minutes
-
-time_spent = 0
-working = False
-
-
-@rumps.clicked('Start Work Cycle')
-def start_work(self):
-    if self.working:
-        print "There is already a work cycle going. Starting a new " + \
-            "one anyway"
-    self.working = True
-
-
-def update_pomodoro(self):
-    self.time_spent += 1
+    @rumps.clicked('Start Work Cycle')
+    def start_work(self, sender):
+        print "Hello world"
 
 
 if __name__ == "__main__":
-    global_namespace_timer = rumps.Timer(tester, 4)
-    rumps.App('fuuu', menu=('Start Work Cycle')).run()
+    Pomodoro().run()
