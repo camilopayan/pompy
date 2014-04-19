eea8cba531c96c867717f91c60e361ff64a8ca02
Camilo Payan <cpayan@gmail.com>
2014-04-18 21:37:32 -0400

    Writing up the workcycle method. I don't think it works :D

--- pom.py
+++ pom.py
@@ -4,10 +4,18 @@ import rumps
 class Interval(rumps.Timer):
 
     interval_length = 1
+    intervals_passed = 0
 
     def __init__(self, length, callback):
         super(self, callback, 5)
         self.interval_length = length
+        self.intervals_passed = 0
+
+    def increment_intervals(self):
+        self.intervals_passed += 1
+
+    def finished(self):
+        return self.intervals_passed == self.interval_length
 
 
 class Pomodoro(rumps.App):
@@ -21,7 +29,16 @@ class Pomodoro(rumps.App):
     @rumps.clicked('Start Work Cycle')
     def start_work(self, sender):
         print "Hello world"
+        Interval(self.worklength, self.work_timer_callback).start()
 
+    def work_timer_callback(self, sender):
+        print "work_timer_callback"
+        sender.increment_intervals()
+        if sender.finished():
+            sender.stop()
+
+    def rest_timer_callback(self, sender):
+        return
 
 if __name__ == "__main__":
     Pomodoro().run()
