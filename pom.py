e21f2c13a98fdc4cb0e471b4196867c18f23baa7
Camilo Payan <cpayan@gmail.com>
2014-04-22 17:41:56 -0400

    Can you believe this is my first working commit in a week? Like it actually runs?

--- pom.py
+++ pom.py
@@ -1,32 +1,47 @@
 import rumps
 
-worklength = 10
-restlength = 5
-working = resting = False
-intervals_done = 0
-pomodoros_done = 0
 
 @rumps.timer(5)
-def work_timer_callback(sender):
-    print "work_timer_callback"
-    if working or resting:
-        intervals_done += 1
-    if working and intervals_done==worklength:
-        working = False
-        resting = True
-        intervals_done = 0
-    elif resting and intervals_done==restlength:
-        working = False
-        resting = False
-        intervals_done = 0
+def pom(sender):
+    print "pom callback"
+    """
+    if app.working and app.intervals_done == app.worklength:
+        app.working = False
+        app.resting = True
+        app.intervals_done = 0
+        print "Work cycle done, beginning rest cycle"
+    elif app.resting and app.intervals_done == app.restlength:
+        app.working = False
+        app.resting = False
+        app.intervals_done = 0
+        app.pomodoros_done += 1
+        print "Rest cycle done"
+    elif app.working or app.resting:
+        app.intervals_done += 1
+        print "working = " + app.working
+        print "resting = " + app.resting
+        print "intervals_done = " + app.intervals_done
+    else:
+        print "Idling"
+    """
 
 
-@rumps.clicked('Start Work Cycle')
-def start_work(self, sender):
-    print "Hello world"
-    
-    
+class Pompy(rumps.App):
+    worklength = 10
+    restlength = 5
+    working = resting = False
+    intervals_done = 0
+    pomodoros_done = 0
+
+    def __init__(self):
+        super(Pompy, self).__init__("P")
+        self.menu = ['Start Work Cycle', None]
+
+    @rumps.clicked('Start Work Cycle')
+    def start_work(self, sender):
+        app.working = True
 
 if __name__ == "__main__":
     rumps.debug_mode(True)
-    rumps.App('P',  menu=['Start Work Cycle']).run()
+    app = Pompy()
+    app.run()
