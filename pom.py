8b86c97601dc7d3a8cf0d1f81eb7068cea6666ca
Camilo Payan <cpayan@gmail.com>
2014-04-21 21:06:32 -0400

    Apparently I still haven't gone simple enough. Considering learning Objective C.

--- pom.py
+++ pom.py
@@ -1,44 +1,32 @@
 import rumps
 
+worklength = 10
+restlength = 5
+working = resting = False
+intervals_done = 0
+pomodoros_done = 0
 
-class Interval(rumps.Timer):
-
-    interval_length = 1
-    intervals_passed = 0
-
-    def __init__(self, length, callback):
-        super(self, callback, 5)
-        self.interval_length = length
-        self.intervals_passed = 0
-
-    def increment_intervals(self):
-        self.intervals_passed += 1
-
-    def finished(self):
-        return self.intervals_passed == self.interval_length
+@rumps.timer(5)
+def work_timer_callback(sender):
+    print "work_timer_callback"
+    if working or resting:
+        intervals_done += 1
+    if working and intervals_done==worklength:
+        working = False
+        resting = True
+        intervals_done = 0
+    elif resting and intervals_done==restlength:
+        working = False
+        resting = False
+        intervals_done = 0
 
 
-class Pomodoro(rumps.App):
-    worklength = 10
-    restlength = 5
-
-    def __init__(self):
-        super(Pomodoro, self).__init__('P', menu=['Start Work Cycle'])
-        rumps.debug_mode(True)
-
-    @rumps.clicked('Start Work Cycle')
-    def start_work(self, sender):
-        print "Hello world"
-        Interval(self.worklength, self.work_timer_callback).start()
-
-    def work_timer_callback(self, sender):
-        print "work_timer_callback"
-        sender.increment_intervals()
-        if sender.finished():
-            sender.stop()
-
-    def rest_timer_callback(self, sender):
-        return
+@rumps.clicked('Start Work Cycle')
+def start_work(self, sender):
+    print "Hello world"
+    
+    
 
 if __name__ == "__main__":
-    Pomodoro().run()
+    rumps.debug_mode(True)
+    rumps.App('P',  menu=['Start Work Cycle']).run()
